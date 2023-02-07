c6b216d382b7a91a5b96a9b9f1bb8407bc5ac211
Saurav Gupta <sauravgupta362@gmail.com>
2023-02-07 17:06:54 +0530

    Problem2

---
+++ hw5.js
@@ -0,0 +1,23 @@
+// Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday"
+
+
+let a = "Sun";
+
+let  b= "Mon";
+
+switch(a && b){
+
+  case "Sun":
+
+    console.log("Sunday");
+    break;
+
+  case " Mon":
+
+    console.log("Monday");
+
+  break;
+
+    default :
+     console.log("No day");
+}
